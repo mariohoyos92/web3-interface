8a73a01da0a5e9341dd3e923986b64310ed74111
Mario Hoyos <mariohoyos92@gmail.com>
2018-10-03 13:17:53 -0500

    refactor: break wallets out into their own table

--- db/migrations/20180925100932_ico.js
+++ db/migrations/20180925100932_ico.js
@@ -11,10 +11,14 @@ exports.up = function (knex, Promise) {
             .boolean("netki_approved")
             .notNullable()
             .defaultTo(false);
-        table.string("public_eth_address").nullable();
-    })
+    }).then(() => knex.schema.createTable("wallets", table => {
+        table.increments("wallet_id").unsigned().primary();
+        table.integer("user_id").references("ico.id");
+        table.string("public_eth_address").notNullable();
+        table.boolean("is_whitelisted").notNullable().defaultTo(false);
+    }))
 }
 
 exports.down = function (knex, Promise) {
-    return knex.schema.dropTable("ico");
+    return knex.schema.dropTable("ico").then(() => knex.schema.dropTable("wallets"))
 };
